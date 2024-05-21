0873aad13ef26b5edba560c36933c0152e0535f3
JHANPIERRE <jmunozmanayay@gmail.com>
2024-05-20 22:32:13 -0500

    Se corrigio la opcion de Registrar y Editar y se agrego la funcionalidad del modal para la eliminación.

--- app_ventas_501/Controllers/PersonalController.cs
+++ app_ventas_501/Controllers/PersonalController.cs
@@ -26,67 +26,72 @@ namespace app_ventas_501.Controllers
             return Json(new { data = arr_personal });
         }
 
-        [HttpGet]
-        public JsonResult Consultar(String dni)
-        {
-            String cad_sql = "exec sp_consultar_personal @dni";
-            Personal personal = new Personal();
-            personal = _context.Personal.FromSqlRaw(cad_sql, new SqlParameter("@dni", dni)).ToList().FirstOrDefault();
-            return Json(personal);
-        }
+		[HttpGet]
+		public JsonResult Consultar(String dni)
+		{
+			String cad_sql = "exec sp_consultar_personal @dni";
+			Personal? personal = new Personal();
+			personal = _context.Personal.FromSqlRaw(cad_sql, new SqlParameter("@dni", dni)).ToList().FirstOrDefault();
+			return Json(personal);
+		}
 
-        [HttpPost]
-        public IActionResult Grabar([FromBody] Personal personal)
-        {
-            bool rpta = true;
-            try
-            {
-                Personal tmp_personal = null;
-                tmp_personal = (from per in _context.Personal
-                                where per.dni == personal.dni
-                                select per).FirstOrDefault();
-                if (tmp_personal != null)
-                {
-                    _context.Personal.Add(personal);
-                    _context.SaveChanges();
-                }
-                else
-                {
-                    tmp_personal.ap_paterno = personal.ap_paterno;
-                    tmp_personal.ap_materno = personal.ap_materno;
-                    tmp_personal.nombre = personal.nombre;
-                    tmp_personal.genero = personal.genero;
-                    tmp_personal.fecha_nacimiento = personal.fecha_nacimiento;
-                    tmp_personal.sueldo = personal.sueldo;
+		[HttpPost]
+		public IActionResult Grabar([FromBody] Personal personal)
+		{
+			bool rpta = true;
 
-                    _context.SaveChanges();
-                }
-            }
-            catch (Exception ex)
-            {
-                rpta = false;
-            }
-            return Json(new {resultado = rpta});
-        }
+			try
+			{
+				Personal? tmp_personal = null;
 
-        public JsonResult Borrar(String dni)
-        {
-            bool rpta = true;
-            try
-            {
-                Personal personal = new Personal();
-                personal = (from per in _context.Personal
-                                where per.dni == dni
-                                select per).FirstOrDefault();
-                _context.Personal.Remove(personal);
-                _context.SaveChanges();
-                
-            }
-            catch (Exception ex)
-            {
-                rpta = false;
-            }
-            return Json(new { resultado = rpta });
-        }
-    }
+				tmp_personal = (from per in _context.Personal
+								where per.dni == personal.dni
+								select per).FirstOrDefault();
+
+				if (tmp_personal == null)
+				{
+					_context.Personal.Add(personal);
+					_context.SaveChanges();
+				}
+				else
+				{
+					tmp_personal.ap_paterno = personal.ap_paterno;
+					tmp_personal.ap_materno = personal.ap_materno;
+					tmp_personal.nombre = personal.nombre;
+					tmp_personal.genero = personal.genero;
+					tmp_personal.fecha_nacimiento = personal.fecha_nacimiento;
+					tmp_personal.sueldo = personal.sueldo;
+
+					_context.SaveChanges();
+				}
+
+			}
+			catch (Exception ex)
+			{
+				rpta = false;
+			}
+
+			return Json(new { resultado = rpta });
+		}
+
+		public JsonResult Borrar(String dni)
+		{
+			bool rpta = true;
+			try
+			{
+				Personal? personal = new Personal();
+				personal = (from per in _context.Personal
+							where per.dni == dni
+							select per).FirstOrDefault();
+				_context.Personal.Remove(personal);
+				_context.SaveChanges();
+
+			}
+			catch (Exception ex)
+			{
+				rpta = false;
+			}
+			return Json(new { resultado = rpta });
+		}
+	}
 }
